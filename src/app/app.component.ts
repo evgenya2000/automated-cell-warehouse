0608f4352756fd54072893f83848129d0951829a
evgenya2000 <evgenya.2000@yandex.ru>
2023-09-25 10:05:11 +0300

    Добавлена форма для задания  значений стеллажей, полок, ячеек + добалено плавное открытие ячейки

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numOfRows: number = 15;
-  numOfShelves: number = 3;
-  numOfCells: number = 5;
+  numOfRows: number;
+  numOfShelves: number;
+  numOfCells: number;
   
-  rows: number[];
-  shelves: number[];
-  cells: number[];
+  rows: number[] | undefined;
+  shelves: number[] | undefined;
+  cells: number[] | undefined;
 
   constructor() {
-    this.rows = Array.from({length: this.numOfRows}, (_, i) => i + 1);
-    this.cells = Array.from({length: this.numOfCells}, (_, i) => i + 1);
-    this.shelves = Array.from({length: this.numOfShelves}, (_, i) => i + 1);
+    this.numOfRows = 3;
+    this.numOfShelves = 3;
+    this.numOfCells = 3;
+    this.renderRows();
+  }
+
+  renderRows() {
+    this.rows = Array.from({ length: this.numOfRows }, (_, i) => i + 1);
+    this.cells = Array.from({ length: this.numOfCells }, (_, i) => i + 1);
+    this.shelves = Array.from({ length: this.numOfShelves }, (_, i) => i + 1);
+    alert(`Создана мнемосхема стелажа. Кол-во стеллажей: ${this.numOfRows}, кол-во полок: ${this.numOfShelves}, кол-во ячеек: ${this.numOfCells}`);
   }
 }
